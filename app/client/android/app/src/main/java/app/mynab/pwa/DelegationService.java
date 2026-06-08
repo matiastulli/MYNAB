008b7573ffdb579574cd02e7354e3dc6927f691d
Juan Matias Tulli <jmatiastulli@gmail.com>
2026-06-08 14:37:24 -0300

    TWA app

---
+++ app/client/android/app/src/main/java/app/mynab/pwa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.mynab.pwa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
